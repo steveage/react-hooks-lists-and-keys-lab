d33895b170133e434b09eee1b16f9af6731d863e
Mariusz <mariuszbia@yahoo.com>
2022-06-05 17:32:49 -0500

    Completed assignment.

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,10 +6,14 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {getUiSpans(technologies)}
       </div>
     </div>
   );
+}
+
+function getUiSpans(technologies) {
+  return technologies.map(technology => <span key={technology}>{technology}</span>);
 }
 
 export default ProjectItem;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,15 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {getProjectItemUi(projects)}
+      </div>
     </div>
   );
+}
+
+function getProjectItemUi(projects) {
+  return projects.map(project => <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>);
 }
 
 export default ProjectList;
